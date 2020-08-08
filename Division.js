992e7ce5356dd79cef08a8792c65cfe314f7be1b
Ironman280 <66196004+Ironman280@users.noreply.github.com>
2020-08-08 11:39:31 +0530

    Add files via upload

---
+++ Division.js
@@ -0,0 +1,17 @@
+class Division{
+    constructor(x,y,w,h){
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.w = w;
+        this.h = h;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("purple");
+        rect(pos.x,pos.y, this.w,this.h)
+    }
+};
